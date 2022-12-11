64473c2a07238084f037da6c71b05122c6e7dc92
Marcos Vinícius <101370923+DeVinc1@users.noreply.github.com>
2022-12-11 09:18:11 -0300

    Adicionando enunciado ao exercício

--- exercicio9.java
+++ exercicio9.java
@@ -1,3 +1,8 @@
+/* 
+    Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+        - C = 5 * ((F-32) / 9). 
+*/
+
 import java.util.Scanner;
 
 public class exercicio9 {
