3280249ec8eee1c06f9c0877a2a960c690c84512
Marcos Vinícius <101370923+DeVinc1@users.noreply.github.com>
2022-12-11 09:17:24 -0300

    Adicionando enunciado ao exercício

--- exercicio8.java
+++ exercicio8.java
@@ -1,3 +1,5 @@
+/* Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês. */
+
 import java.util.Scanner;
 
 public class exercicio8 {
