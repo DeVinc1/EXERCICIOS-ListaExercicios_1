ce69354fd20cf0252131e311a858f91044df2484
Marcos Vinícius <101370923+DeVinc1@users.noreply.github.com>
2022-12-11 08:56:12 -0300

    Adicionando enunciado ao exercício

--- exercicio1.java
+++ exercicio1.java
@@ -1,3 +1,5 @@
+/* Faça um Programa que mostre a mensagem "Alo mundo" na tela. */
+
 public class exercicio1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Alô, mundo!");
